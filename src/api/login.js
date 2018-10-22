52db656d6166b5e7cc311893a730eb9eaad1cc6b
H.J.Yang <1501587673@qq.com>
2018-10-23 00:40:40 +0800

    http request

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 
 export function login(username, password) {
+  console.log(request)
   return request({
     url: 'login',
     method: 'post',
--- src/utils/request.js
+++ src/utils/request.js
@@ -69,4 +69,70 @@ service.interceptors.response.use(
   }
 )
 
+const base = ''
+export const postRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    transformRequest: [
+      function(data) {
+        let ret = ''
+        for (const it in data) {
+          ret +=
+            encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        return ret
+      }
+    ],
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+}
+
+export const uploadFileRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+}
+export const putRequest = (url, params) => {
+  return axios({
+    method: 'put',
+    url: `${base}${url}`,
+    data: params,
+    transformRequest: [
+      function(data) {
+        let ret = ''
+        for (const it in data) {
+          ret +=
+            encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        return ret
+      }
+    ],
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+}
+
+export const deleteRequest = url => {
+  return axios({
+    method: 'delete',
+    url: `${base}${url}`
+  })
+}
+export const getRequest = url => {
+  return axios({
+    method: 'get',
+    url: `${base}${url}`
+  })
+}
+
 export default service
